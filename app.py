f59735e6731941a4a8b982c133801a933fd8325c
duvalg <gduval@student.42.fr>
2018-05-12 21:38:30 +0200

    adding the base of a proper sqlite3 connection

--- app.py
+++ app.py
@@ -1,18 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys, traceback
 from tkinter import *
 from tkinter.messagebox import *
 from tkinter.filedialog import *
 
-import sqlite3                      # needed to connect the program to a SQL database
+# Local scripts
+from sqlgestion import *
 
-filename = "sample.txt"             # filename: path to the text file that contains databases's path
+filename = "sample.txt"
 
 _window = Tk()
 _dbList = []
 
-# File reading-writing gestion
+_db = createDatabase("databases_list.db")
+if _db == None: # check if database was successfully created or connected
+    sys.exit(0)
+_cursor = getCursor(_db)
+if _cursor == None: # check if getCursor() encountered an error
+    sys.exit(0)
+
 def readfile(_dbList, liste):
     with open(filename, "r") as fd:
         fd.seek(0)
@@ -20,9 +28,7 @@ def readfile(_dbList, liste):
 
         while line:
             _dbList.append(line.strip())
-            print("file input : " + line.strip()) # debug
             line = fd.readline()
-        print("\n")
     displayList(_dbList, liste)
 
 def rewriteFile(_dbList, ignored):
@@ -31,7 +37,6 @@ def rewriteFile(_dbList, ignored):
     
     for line in _dbList:
         if (i != ignored):
-            print("rewriteFile = " + line)
             fd.write(line + "\n")
         i += 1
 
@@ -40,36 +45,40 @@ def displayList(_dbList, liste):            # displayList: display the list of t
 
     liste.delete(0, END)
     for path in _dbList:
-        print("displayList = " + path)
         liste.insert(i, path)
-    print("\n")
+        i += 1
     liste.pack()
 
-def insertDatabase(_dbList, liste):         # insertDatabase: allow user to get the path of the databases he want to add
+def insertDatabase(_dbList, liste, _cursor):         # insertDatabase: allow user to get the path of the databases he want to add
     filename = askopenfilename(title="Get file's path",filetypes=[('all files','.*')])
 
     if (filename):
+        insertDb = ('''
+        INSERT INTO databases(name, path) VALUES(?, ?)
+        ''')
+        _cursor.execute(insertDb, [(os.path.basename(filename)), (filename)])
+        _db.commit()
         _dbList.append(filename)
         rewriteFile(_dbList, -1)
         del _dbList[:]
         readfile(_dbList, liste)
     
-def removeDatabase(liste, _dbList):  # insertDatabase: allow user to get the path of the databases he want to add
+def removeDatabase(liste, _dbList):         # insertDatabase: allow user to get the path of the databases he want to add
     selectedInput = liste.curselection()
     
     if (selectedInput):
         if askyesno('Are you sure ?', 'Do you really want to delete this database ?'):
-            index = selectedInput[0]    # get the index of the undesired data
+            index = selectedInput[0] # get the index of the undesired data
             rewriteFile(_dbList, index)
             del _dbList[:]
             readfile(_dbList, liste)
 
-def initMenu(_window):               # initMenu: init interface's menu
+def initMenu(_window):                      # initMenu: init interface's menu
     menubar = Menu(_window)
 
     tools = Menu(menubar, tearoff=0)
-    tools.add_command(label="Insert a database", command= lambda: insertDatabase(_dbList, liste))
-    tools.add_command(label="Remove database", command= lambda: insertDatabase(_dbList, liste))
+    tools.add_command(label="Insert a database", command= lambda: insertDatabase(_dbList, liste, _cursor))
+    tools.add_command(label="Remove database", command= lambda: removeDatabase(_dbList, liste))
     tools.add_separator()
     tools.add_command(label="Exporting database")
     tools.add_separator()
@@ -80,7 +89,7 @@ def initMenu(_window):               # initMenu: init interface's menu
 initMenu(_window)
 
 # Add a database (button)
-bouton=Button(_window, text="Insert a database", command= lambda: insertDatabase(_dbList, liste)).pack(pady=25, padx=50)
+bouton=Button(_window, text="Insert a database", command= lambda: insertDatabase(_dbList, liste, _cursor)).pack(pady=25, padx=50)
 
 # Initializing databases (listBox)
 liste = Listbox(_window, width=100)
